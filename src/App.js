eaf512b9e2c10957a80a7e55eefd3f69bd8e0c11
abraidotti <alessandro.braidotti@gmail.com>
2020-01-06 12:03:52 -0500

    add more layout, request, and secrets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar"
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const response = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({ users: response.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
-      <Navbar title="Github Finder" icon="fab fa-github"/>
-        <h1>Hello from React</h1>
+        <Navbar title="Github Finder" icon="fab fa-github" />
+        <div className="container">
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
       </div>
     );
   }
