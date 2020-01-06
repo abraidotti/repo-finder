df36fabc15026dd3ef9a0c7568d7472eb604949a
abraidotti <alessandro.braidotti@gmail.com>
2020-01-06 10:53:51 -0500

    clean up CRA, add basic navbar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar"
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar title="Github Finder" icon="fab fa-github"/>
+        <h1>Hello from React</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
